082dc37a67d781e805bdd7dbba097ba55d146399
farexeezy <122971574+farexeezy@users.noreply.github.com>
2023-03-15 12:45:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+ 
 app = Flask(__name__)
-
-@app.route('/index.html')
+ 
+menu = ["Установка", "Первое приложение", "Обратная связь"]
+ 
+@app.route("/")
 def index():
-    return '.'
+    print( url_for('index') )
+    return render_template('index.html', menu = menu)
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
